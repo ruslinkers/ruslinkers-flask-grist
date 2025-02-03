40de9e17ca73119094d3521785b46a1399f129dc
Oleg Belyaev <obelyaev@gmail.com>
2025-02-04 00:53:53 +0300

    add code for including diachronic data

--- ruslinkers/database.py
+++ ruslinkers/database.py
@@ -7,6 +7,16 @@ db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
 
+import csv
+# Generate the dictionary of lists of dictionaries, for diachronic db
+diachronic = { }
+with open('ruslinkers-dia-1.0.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        modforms = row.split('; ')
+        for form in modforms:
+            diachronic.setdefault(form, []).append(row)
+
 # def init_db():
 #     # import all modules here that might define models so that
 #     # they will be registered properly on the metadata.  Otherwise
